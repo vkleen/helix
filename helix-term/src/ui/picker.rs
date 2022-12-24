7dfeee4cd33388701af54267513e9700325ae2b6
Michael Davis <mcarsondavis@gmail.com>
2022-12-24 15:20:26 -0600

    FilePicker: Allow setting an initial search query
    This can be used to set an initial query from a register. Global
    search will use this and the file picker could also consider allowing
    an initial query using this helper.

--- helix-term/src/ui/picker.rs
+++ helix-term/src/ui/picker.rs
@@ -136,6 +136,11 @@ impl<T: Item> FilePicker<T> {
         self
     }
 
+    pub fn with_line(mut self, line: String, editor: &Editor) -> Self {
+        self.picker.set_line(line, editor);
+        self
+    }
+
     fn current_file(&self, editor: &Editor) -> Option<FileLocation> {
         self.picker
             .selection()
@@ -567,6 +572,10 @@ impl<T: Item> Picker<T> {
             self.score();
         }
         EventResult::Consumed(None)
+    }
+
+    pub fn set_line(&mut self, line: String, editor: &Editor) {
+        self.prompt.set_line(line, editor);
     }
 }
 
